aeba75815db671ae2e12d27fe09d00d7d4d80f7f
Hayk Baghdasarian <baghdasarianhayk@gmail.com>
2025-11-10 23:02:36 +0400

    change tg user id

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ MQ_ROUTING_KEY = "news"
 
 # Telegram Bot Configuration
 BOT_TOKEN = "8497899812:AAEO3PabXI5NPLkDLLo2yR6bNIF01XgUCgc"
-TELEGRAM_USER_ID = 123456789  # Replace with your Telegram user ID
+TELEGRAM_USER_ID = 796973748  # Replace with your Telegram user ID
 WEBHOOK_HOST = "https://owkya-78-109-72-68.a.free.pinggy.link"
 WEBHOOK_PATH = "/bots/webhook"
 WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
